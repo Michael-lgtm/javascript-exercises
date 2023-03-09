0ee20a5bfc7e38520b0336fce7d5987824ee8116
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-08 22:44:56 -0600

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,31 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let end = '';
+    if (num<0) {
+        end = 'ERROR'
+        return end;
+    } else if (num===0) {
+        end = '';
+        return end;
+    } else if (num>0) {
+        let i = 0;
+        while (i<num) {
+            end += string;
+            i++;
+        }
+        return end;
+    } else if (string === '') {
+        end = '';
+        return end;
+    }    
+        else {
+        let i =0;
+        while (i<num) {
+            end += string;
+            i++;
+        }
+        return end;
+    }
 };
-
+repeatString()
 // Do not edit below this line
 module.exports = repeatString;
