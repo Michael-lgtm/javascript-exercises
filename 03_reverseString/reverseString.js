11f63c3a54481c7e6ee4c4c755b5cff73c908a9f
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-09 18:48:29 -0600

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let str = text;
+    let newStr = "";
+    let arr = [];
+    for ( let i = 0; i < str.length;i++) {
+        arr.push(str[i])
+    }
+    console.log(arr.length)
+    arr.reverse()
+    for (let i = 0; i < arr.length; i++) {
+        newStr += arr[i]
+    }
+    return newStr;
 };
-
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
