dbe75538e0d27c76da23b0798526bb315cc0bd78
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-09 21:16:55 -0600

    Completed 04_removeFromArray (all of them this time lol)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,19 @@
-const removeFromArray = function(input, itemQuery) {
-    let arr = input;
-    let item = "";
-    const count = 50;
-    if (itemQuery.length > 50 ) {
-        console.log("LIMIT IS 50 CHARACTERS!")
-    } else {
-        for ( i = 0; i<count ; i++ ) {
-            if (arr[i] === itemQuery) {
-                item = arr[i]
-                arr.splice(arr[i] -1, 1)
-                item = arr;
-                break;
-            } else if (arr[i] != itemQuery && arr[i] == arr[-1]) {
-                item = "CAN NOT FIND ITEM!"
-                break;
-            }
-            else {
-                console.log("searching")
-                continue;
+const removeFromArray = function(input, ...args) {
+    let userArr = input;
+    let newArr = [];
+    for (d = 0;d<args.length;d++) {
+        for (i = 0;i<userArr.length;i++) {
+            if (userArr[i] === args[d]) {
+                userArr.splice(i, 1)
+                newArr = userArr
+            } else if (userArr[i] != args[d]) {
+                newArr = userArr
             }
         }
     }
-    return item
-};
-removeFromArray([1, 2, 3, 4], 2)
+    return newArr;
+}
+    
+removeFromArray([1, 2, 3, 4], 3, "tesy")
 // Do not edit below this line
 module.exports = removeFromArray;
