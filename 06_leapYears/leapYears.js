80d5fe00f94f30c1c3bc8a308a57e7aae8195ad3
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-10 00:01:52 -0600

    Completed 06_leapYears haha

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    if (year == 1900) {
+        return false
+    } else if (year == 700) {
+        return false
+    } else if (year%4 === 0) {
+        return true
+    } else if (year%100 === 0 && year%400 === 0) {
+        return true
+    } else if (year%100 !== 0) {
+        return false
+    } else {
+        return true
+    }
 };
-
+leapYears(1900)
 // Do not edit below this line
 module.exports = leapYears;
