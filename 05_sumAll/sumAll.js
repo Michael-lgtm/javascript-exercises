9bf6a5a1c66474f9d29019087bd28e9fa94517c1
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-09 23:08:17 -0600

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof start != "number" || typeof end != "number") {
+        return 'ERROR'
+    } else if (start<0 || end<0) {
+        return 'ERROR'
+    } else if (end > start) {
+        for (let i = 0; i<end-(start - 1); i++) {
+            sum += start + i;
+        }
+        return sum
+    } else if (start > end) {
+        for (let i = 0; i<start-(end - 1); i++) {
+            sum += end + i;
+        }
+        return sum
+    }
 };
-
+sumAll(1, "2")
 // Do not edit below this line
 module.exports = sumAll;
+``
