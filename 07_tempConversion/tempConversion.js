defa59ac662ecc6b6afa78a18213d0cef414e718
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-10 00:25:00 -0600

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInC = (5/9*(temp-32))
+  return parseFloat(tempInC.toFixed(1))
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInF = ((9/5)*temp+32)
+  return parseFloat(tempInF.toFixed(1))
 };
-
+convertToCelsius(100)
+convertToFahrenheit(0)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
