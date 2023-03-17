bbd4223cff56990b549388fb15e98f585e4763cf
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-16 22:41:02 -0500

    Completed 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arrOne = []
+    let validatingStr = ""
+    let arrTwo = []
+    let secondStr = ""
+    for (let i = 0; i < str.length; i++) {
+        arrOne.push(str[i]);
+    }
+    for (let i = 0; i < arrOne.length; i++) {
+        if (arrOne[i].match(/[a-z]/i)) {
+            validatingStr += arrOne[i]
+        }
+    }
+    validatingStr = validatingStr.toLowerCase()
+    for (let i = 0; i < validatingStr.length; i++) {
+        arrTwo.push(validatingStr[i]) 
+    }
+    arrTwo.reverse()
+    for (let i = 0; i < arrTwo.length; i++) {
+        secondStr += arrTwo[i]
+    }
+    if (validatingStr === secondStr) {
+        return true
+    } else {
+        return false
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
