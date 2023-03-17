b56f89c364568d5224390c1da4f81d965520543f
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-16 21:09:29 -0500

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  return numOne + numTwo
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+  let newArr = []
+  for (let each of args) {
+    newArr.push(each)
+  }
+  if (newArr.length == 0) {
+    return 0
+  } else if (newArr.length == 1) {
+    return newArr[0]
+  } else if (newArr.length > 1) {
+    let sum = 0
+    for (let index = 0; index < newArr.length; index++) {
+      sum += newArr[index];
+    }
+    return sum
+  }
 };
 
-const multiply = function() {
-
+const multiply = function([...args]) {
+  let product = 1
+  for (let index = 0; index < args.length; index++) {
+    product = product * args[index];
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+  let power = 1;
+  for (let index = 0; index < numTwo; index++) {
+    power = power * numOne
+  }
+  return power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr = []
+  for (let index = 1; index < num + 1; index++) {
+    arr.push(index)    
+  }
+  let product = 1
+  for (let index = 0; index < arr.length; index++) {
+    product = product * arr[index];
+  }
+  return product
 };
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
