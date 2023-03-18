d7168c1654981e8a2e52eac63c2437130fb9a65c
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-18 13:12:25 -0500

    Completed 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS'
+    } else {
+        let fibSeq = [0, 1]
+        let newNum = 0
+        for (let i = 0; i < num; i++) {
+            newNum = fibSeq[i] + fibSeq[i+1]
+            fibSeq.push(newNum)
+        }
+        let length = fibSeq.length
+        return fibSeq[length - 2]
+    }
 };
-
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
