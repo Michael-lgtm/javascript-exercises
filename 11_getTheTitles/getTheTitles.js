a86fea24fcb3f1dc72a5df259ebaddb2cc890960
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-18 17:26:02 -0500

    Completed 11 getTheTitles Did what I first thought of and using a built-in method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(books) {
+    let newArr = []
+    for (let i = 0; i < books.length; i++) {
+        newArr.push(books[i].title)
+    }
+    return newArr
+    // or use .map()
+    // I just thought of a for loop first
+    // but map already loops through each obj
+    // books.map((book) => book.title)
+}
+getTheTitles(
+    [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+)
 // Do not edit below this line
 module.exports = getTheTitles;
