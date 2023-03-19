7fd1ce632adef4823e575976b5eaffeda3248d01
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-18 23:25:29 -0500

    Successfully completed 12 findTheOldest Learned a lot about the sort() function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let i = 0; i < people.length; i++) {
+            if (people[i].yearOfDeath === undefined) {
+                people[i].yearOfDeath = new Date().getFullYear()
+                console.log(people[i].yearOfDeath)
+            }
+        }
+    people.sort((a, b) => {
+        if ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    return people[0]
 };
-
+findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+    //   yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ])
 // Do not edit below this line
 module.exports = findTheOldest;
